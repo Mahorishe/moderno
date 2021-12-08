69982c0d49ceafde8d61ae841ca1038b826716db
Mahorishe <mahorishe73@gmail.com>
2021-12-08 14:05:01 +0300

    added products-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,17 @@
 $(function(){
-
     let mixer = mixitup('.products__inner-box');
 
+    $('.rate-star').rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true,
+      });
+    
+    $('.products-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
 
+    });
 });
