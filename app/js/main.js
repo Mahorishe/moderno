bb3785ae2920d0fa7ec4bb7673fbac62fec0c31a
Mahorishe <mahorishe73@gmail.com>
2021-12-14 22:58:59 +0300

    adaptive all page

--- app/js/main.js
+++ app/js/main.js
@@ -12,10 +12,34 @@ $(function(){
         arrows: false,
         slidesToShow: 4,
         slidesToScroll: 4,
-
+        responsive: [
+          {
+            breakpoint: 1900,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 1441,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+            }
+          },
+          {
+            breakpoint: 901,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+          },
+        ]
     });
 
-    $(".js-range-slider").ionRangeSlider({
+    $('.js-range-slider').ionRangeSlider({
       type: "double",
       min: 0,
       max: 1000,
